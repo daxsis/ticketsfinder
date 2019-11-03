a070217bac0e1bfbfe537e0757e16c443a0038ce
Daxsis <loop8881@yandex.ru>
2019-11-03 12:04:38 -0500

    Flight Seeder now seeds with correct timzones relatives to the airports

--- database/seeds/FlightsSeeder.php
+++ database/seeds/FlightsSeeder.php
@@ -19,24 +19,30 @@ class FlightsSeeder extends Seeder
 
         $timestamp = Carbon::now();
         $airlineIds = Airline::pluck('id')->toArray();
-//        $airlineIata = Airline::pluck('iata')->toArray();
         $airports = Airport::pluck('id')->toArray();
 
         $flights = array();
         for($i = 0; $i <= 500; $i++) {
             $number = rand(100, 999);
             $airlineId = $airlineIds[array_rand($airlineIds)];
-            $airlineIata = Airline::where('id', $airlineId)->first()->iata;
+            $airline = Airline::where('id', $airlineId)->first();
+            $departure_airport = Airport::where('id', $airports[array_rand($airports)])->first();
+//            dd($departure_airport);
+            $arrival_airport = Airport::where('id', $airports[array_rand($airports)])->first();
 
-            $flights[] = factory(Flight::class)->raw([
+            $flight = factory(Flight::class)->raw([
                 'airline_id' => $airlineId,
                 'number' => $number,
-                'uid' => $airlineIata . $number,
-                'departure_airport' => $airports[array_rand($airports)],
-                'arrival_airport' => $airports[array_rand($airports)],
+                'uid' => $airline->iata . $number,
+                'departure_airport' => $departure_airport->id,
+                'arrival_airport' => $arrival_airport->id,
                 'created_at' => $timestamp,
                 'updated_at' => $timestamp,
             ]);
+            $flight['departure_time'] = Carbon::parse($flight['departure_time'])->setTimezone($departure_airport->timezone)->toDateTimeString();
+            $flight['arrival_time'] = Carbon::parse($flight['arrival_time'])->setTimezone($arrival_airport->timezone)->toDateTimeString();
+
+            array_push($flights, $flight);
         }
 
         Flight::insert($flights);
